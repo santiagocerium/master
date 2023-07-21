24f8f863a8a4669a8db5811d9f12f1ac116cea21
Santiago Cerium <santiagocerium@gmail.com>
2023-07-21 13:07:27 +0200

    release2

--- primero.java
+++ primero.java
@@ -1,11 +1,30 @@
 class primero{
     static private int numero = 33;
+    
+
     public static void main(String[] args) {
         
+        interna a;
+        a = new interna();
+
+        a.entero = 5;
+        a.mostrar();
         int contador = 1;
         System.out.printf("Hola, mundo %d \n", primero.numero);
-        primero.numero = 44;
+        primero.numero = contador;
         System.out.printf("Hola, mundo %d \n", primero.numero);
+
+        System.out.printf("Hola, mundo %d \n", a.entero);
     }
 
 }
+
+class interna {
+        public int entero = 0;
+
+        public interna (){}
+
+        void mostrar () {
+            System.out.printf("ENTERO: %d\n", this.entero);
+        }
+    }
